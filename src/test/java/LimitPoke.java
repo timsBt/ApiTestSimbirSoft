53bde048c79aae4a61639914c6167c5bc9176f7d
timsBt <tims.bekmat@gmail.com>
2023-04-05 00:58:01 +0400

    test tests

---
+++ src/test/java/LimitPoke.java
@@ -0,0 +1,13 @@
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Data;
+
+
+@Data
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class LimitPoke {
+
+        @JsonProperty ("name")
+        private String resultNames;
+
+}
