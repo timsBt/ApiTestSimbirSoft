bbab37ee161b49556c7c825cd49a627763adc5dd
timsBt <tims.bekmat@gmail.com>
2023-04-06 23:27:28 +0400

    allure,pom,parallel,package,testcase

--- src/test/java/Models/NamesPokemons.java
+++ src/test/java/Models/NamesPokemons.java
@@ -1,3 +1,5 @@
+package Models;
+
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
--- src/test/java/Steps.java
+++ src/test/java/Steps.java
@@ -1,3 +1,7 @@
+import Models.Ability;
+import Models.LimitPokemons;
+import Models.NamesPokemons;
+import Models.Weight;
 import io.qameta.allure.Step;
 import java.util.List;
 
@@ -9,16 +13,16 @@ public class Steps {
     private final static String URL = "https://pokeapi.co/";
 
     @Step ("Получение ограниченного списка покемонов")
-    public static List<LimitPokemons> limitPokemonsList() {
+    public static List<LimitPokemons> limitPokemonsList(int count) {
         Spetifications.installSpetification(Spetifications.requestSpec(URL), Spetifications.responseSpecOK200());
         return given()
                 .when()
-                .get("api/v2/pokemon")
+                .get("api/v2/pokemon?limit=" + count + "&offset=0")
                 .then()
                 .extract().jsonPath().getList("results", LimitPokemons.class);
     }
 
-    @Step ("Получение Ability (умение) покемона")
+    @Step ("Получение Models.Ability (умение) покемона")
     public static List<Ability> ability(String name) {
         Spetifications.installSpetification(Spetifications.requestSpec(URL), Spetifications.responseSpecOK200());
         return given()
@@ -28,7 +32,7 @@ public class Steps {
                 .extract().jsonPath().getList("abilities.ability", Ability.class);
     }
 
-    @Step ("Получение Weight (вес) покемона")
+    @Step ("Получение Models.Weight (вес) покемона")
     public static Weight checkWeightPokemonsTests(String name) {
         Spetifications.installSpetification(Spetifications.requestSpec(URL), Spetifications.responseSpecOK200());
         return given()
@@ -38,6 +42,7 @@ public class Steps {
                 .extract().as(Weight.class);
     }
 
+    @Step ("Получение Name (имя) покемона")
     public static NamesPokemons checkNamePokemonsTest (String name){
         Spetifications.installSpetification(Spetifications.requestSpec(URL),Spetifications.responseSpecOK200());
         return given()
