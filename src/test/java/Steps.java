adecce6721906394d5ef891bb3fd46c3bb4fe6a3
Tims <92653720+timsBt@users.noreply.github.com>
2023-04-27 02:17:16 +0400

    Revert "Steps"
    This reverts commit 199226dbaeb11d5ffcb246613970530d0171d8c2.

--- src/test/java/Steps.java
+++ src/test/java/Steps.java
@@ -1,17 +1,8 @@
-import java.util.List;
 
-import static io.restassured.RestAssured.given;
 
 public class Steps {
-    private final static String URL = "https://pokeapi.co/";
 
-    public static List <LimitPoke> pokesName() {
-        Spetifications.installSpetification(Spetifications.requestSpec(URL), Spetifications.responseSpecOK200());
-        return given()
-                .when()
-                .get("api/v2/pokemon?limit=100000&offset=0")
-                .then()
-                .extract().body().jsonPath().getList("results", LimitPoke.class);
-    }
+
+
 
 }
